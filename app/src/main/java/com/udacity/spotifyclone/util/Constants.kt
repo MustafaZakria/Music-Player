f3f2b8c0fc896b3fd6d8022c315418c8008f0be8
Mustafa Zakria <mustafa.zakria21@gmail.com>
2023-02-03 01:48:20 +0200

    setting up the music notification

--- app/src/main/java/com/udacity/spotifyclone/util/Constants.kt
+++ app/src/main/java/com/udacity/spotifyclone/util/Constants.kt
@@ -2,4 +2,7 @@ package com.udacity.spotifyclone.util
 
 object Constants {
     const val SONG_PATH = "songs"
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
 }
